8d9c6b3975fb0e9fdb4f56c7ceda934a0bf2445b
br8832 <bryan2098@hotmail.com>
2022-04-26 17:08:03 -0700

    trying to fix janky navbar on mobile
    half way there. just need to have an icon there to click

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import Footer from './Footer'
 import Education from './Education'
 import Projects from './Projects'
 import Work from './Work'
+import {useState} from 'react'
 import './App.css';
 
 function App() {
+  const [isMobile, setMobile] = useState(false);
+  const toggle = () => setMobile(!isMobile)
   return (
-      <><Intro />
+      <><Intro isMobile={isMobile} toggle={toggle}/>
         <Work/>
         <Projects />
         <Education />
--- src/Intro.js
+++ src/Intro.js
@@ -1,23 +1,52 @@
-import React from 'react';
+import React from 'react'
 import resume from './images/Resume.docx'
 import me from './images/me.jpg'
 class Intro extends React.Component{
     constructor (props){
         super(props);
+        this.state = props.isMobile;
+        this.toggle = props.toggle;
         this.sections = ['about', 'work', 'projects','education', 'contact']
     }
+
     anchorList(){
         return (<nav class="navbar">
             {this.sections.map((item) =>
                 {return <a rel="noreferrer" href={"#"+item}>{item[0].toUpperCase()+item.slice(1)}</a>})}
-            <a class="download" target="_blank" rel="noreferrer" href={resume} download="Bryan_Rodriguez_Resume.docx"><i class="far fa-file-pdf"></i> Download</a>
+            <a class="download" target="_blank" rel="noreferrer" href={resume} download="Bryan_Rodriguez_Resume.docx"><i class="far fa-file-pdf"></i>Download</a>
         </nav>)
     }
+    showMobileMenu(){
+        const border = window.innerWidth < 768
+        let menu = document.getElementsByTagName('aside')[0];
+        menu.style.display = border ? 'inherit' : "none"
+        
+    }
+    clearMenu(){
+            let menu = document.getElementsByTagName('aside')[0];
+            menu.className = "";
+            menu.style.display = 'none'
+    }
+
+    mobileMenu(){
+        return(
+            <aside class="initialMobileMenu">
+                <span onClick={this.clearMenu} class='popup-close'>X</span>
+                <ul>
+                {this.sections.map((item) =>
+                {return <li key={item} onClick={this.clearMenu}><a rel="noreferrer" href={"#"+item}>{item[0].toUpperCase()+item.slice(1)}</a></li>})}
+                </ul>
+            </aside>
+        )
+
+    }
     render(){
+        window.addEventListener('resize',this.showMobileMenu)
         return(
         <header>
-            {this.anchorList()}    
-        <div class="content-wrap">
+            {this.anchorList()}
+            {this.mobileMenu()}    
+        <div style={{paddingTop:"50px"}} class="content-wrap">
             <img id="about" class="profile-img column-narrow" src={me} alt="Your Name"></img>
             <div class="column-wide">
                 <h1>Bryan Rodriguez</h1>
